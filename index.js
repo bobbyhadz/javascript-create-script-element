e08b586b29863dd9eb82cf1f0118f8f5b7b12cb2
bobbyhadz <opmftw@gmail.com>
2024-03-05 14:45:11 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('bobbyhadz.com');
+
+const script = document.createElement('script');
+
+// 👇️ local file
+// script.setAttribute('src', 'another-file.js');
+
+// 👇️ remote file
+script.setAttribute(
+  'src',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+);
+
+script.setAttribute('async', '');
+
+// 👇️ optionally set script to be treated as JS module
+// script.setAttribute('type', 'module');
+
+script.onload = function handleScriptLoaded() {
+  console.log('script has loaded');
+
+  document.getElementById('box').textContent =
+    'Script loaded successfully';
+};
+
+script.onerror = function handleScriptError() {
+  console.log('error loading script');
+};
+
+document.head.appendChild(script);
